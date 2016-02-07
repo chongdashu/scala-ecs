0f025ac27b634a597c22e6c93cf8a4290bbbe705
Chong-U Lim <lim.chong.u@gmail.com>
2016-02-07 13:35:35 -0500

    Update tests

--- src/test/scala/ecs/TestEntity.scala
+++ src/test/scala/ecs/TestEntity.scala
@@ -15,13 +15,32 @@ abstract class TestEntity extends TestCase {
        entity2 = new Entity();
     }
 
+    /**
+     * Tests that an [[com.chongdashu.scala.ecs.Entity]] created using
+     * a constructor with no arguments gets assigned a valid id.
+     */
     def testEntityEmptyConstructor {
         assertNotNull("Entity created should not be Null", entity0);
         assertEquals(Entity.KEY_PREFIX + "0",  entity0.key);
     }
     
+    /**
+     * Tests that two [[com.chongdashu.scala.ecs.Entity]] objects
+     * created using constructors with no arguments are both assigned
+     * a valid id, which are different.
+     */
     def testEntityCreateTwo {
         assertEquals(Entity.KEY_PREFIX + "0",  entity0.key);
         assertEquals(Entity.KEY_PREFIX + "1",  entity1.key);
+    }
+    
+    /**
+     * Tests that creating an [[com.chongdashu.scala.ecs.Entity]] object
+     * with a specified `key` results in one created with that 
+     * `Entity.key` assigned.correctly.
+     */
+    def testEntityCreateSpecifiedKey {
+        val entitySpecial = new Entity("Special");
+        assertEquals("Special", entitySpecial.key);
     }
 }
